86e22b872174b16471d5c62d33b03f6ee2527e54
cliffmieser <cliffmieser@gmail.com>
2021-08-14 14:34:35 -0400

    added colormap

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,9 +1,14 @@
 #scatter_squares.py
 import matplotlib.pyplot as plt
 
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
+
+
 plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200) # 's' sets the size of the dot in this scatter method
+ax.scatter(x_values, y_values, c = y_values, cmap=plt.cm.Blues, s=10) # 's' sets the size of the dot in this scatter method
 
 #set chart title and label axes
 ax.set_title("Square Numbers", fontsize = 24)
@@ -13,5 +18,13 @@ ax.set_ylabel("Square of Value", fontsize = 14)
 #Size set for tick labels
 ax.tick_params(axis = 'both', which = 'major', labelsize =14)
 
+#set range for each axis
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
+
+
+#below saves plots autpmatically in an image
+"""
+plt.savefig('squares_plot.png', 'bbox_inches='tight')
+"""
