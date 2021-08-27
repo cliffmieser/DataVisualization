bd3d4917d12a2043c678212cb4a9a1b863761a99
cliffmieser <cliffmieser@gmail.com>
2021-08-26 22:58:42 -0400

    Color scales

--- Data/Mapping global data sets/eq_world_map.py
+++ Data/Mapping global data sets/eq_world_map.py
@@ -8,20 +8,22 @@ from plotly import offline
 
 
 
-filename = 'eq_data_1_day_m1.json'
+filename = 'eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
     
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
     
 #map earthquakes
@@ -29,9 +31,14 @@ data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
-        'size': [5*mag for mag in mags],
-        }
+        'size': [3*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'}
+        },
 
     }]
 my_layout = Layout(title = 'Global Earthquakes')
---
+++ Data/Mapping global data sets/show_color_scales.py
@@ -0,0 +1,6 @@
+#show_color_scales.py
+
+from plotly import colors
+
+for key in colors.PLOTLY_SCALES.keys():
+    print(key)
